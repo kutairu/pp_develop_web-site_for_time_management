a2bddd56c625c847b0888feff259db336f970b2e
kutairu <bzloman21@gmail.com>
2026-06-17 22:52:03 +0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Эта строчка превращает Next.js в статику
+  images: {
+    unoptimized: true, // Обязательно для GitHub Pages
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
